accea5b55f57c22faf1866f8ff7d960126fa3cfd
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-05-07 00:21:05 +0200

    Update User_interface.py

--- Code/User_interface.py
+++ Code/User_interface.py
@@ -1,7 +1,12 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, QToolBar, QStatusBar, QLabel, QAction, QWidget, QHBoxLayout, QVBoxLayout, QSpacerItem, QSizePolicy, QFrame
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, QToolBar, QStatusBar, QLabel, QAction, QWidget, QHBoxLayout, QVBoxLayout, QSpacerItem, QSizePolicy, QFrame, QActionGroup
 from PyQt5.QtGui import QPixmap, QIcon, QCursor, QFont,QPainter, QColor
 from PyQt5.QtCore import Qt, QByteArray, QObject, QEvent, pyqtSignal, QRect, QSize, QPoint
 from Icon_svg import *
+# from os import path as opath
+# from os import environ as oenviron
+import os
+import json
+
 
 class WinEventFilter(QObject):
     def eventFilter(self, obj, event):
@@ -23,6 +28,7 @@ class Ui_MainWindow(QMainWindow):
         self.event_filter = WinEventFilter()
         self.installEventFilter(self.event_filter)
         self.setContextMenuPolicy(Qt.NoContextMenu)
+        # self.connections_Ui_MainWindow()
     
     def ui_init(self):
         self.setWindowTitle("截图小工具")
@@ -30,15 +36,46 @@ class Ui_MainWindow(QMainWindow):
         self.label_init()
     
     def parameter_init(self):
-        temp_list = []
+        self.screens_info_list = []
         self.screens_info_in_label_text = ''
-        self.screen_size_all = QApplication.screens()
-        for i in range(len(self.screen_size_all)):
-            temp_list.append(self.screen_size_all[i].size())
-            self.screens_info_in_label_text += f' 第{i+1}屏幕尺寸：({self.screen_size_all[i].size().width()}, {self.screen_size_all[i].size().height()})'
-        self.screen_size_tuple = tuple(temp_list)
-        del temp_list
-        # print(QApplication.screens()[1].geometry())
+        self.app_setting = {
+            "screenshot_screen": 0
+        }
+        self.read_setting_file()
+        self.get_screens_info()
+    
+    # def connetions_Ui_MainWindow(self):
+    #     for _, item in enumerate(self.all_screen_select_menu_action_list):
+    #         item.triggel
+    def read_setting_file(self):
+        user_appdata_path = os.path.join(os.environ.get('USERPROFILE'), 'AppData', 'Local', 'Screen_Measure')
+        if not os.path.exists(user_appdata_path):
+            os.makedirs(user_appdata_path)
+        setting_file_path = os.path.join(user_appdata_path, 'Screen_Measure_setting')
+        if not os.path.exists(setting_file_path):
+            with open(setting_file_path, 'w', encoding='utf-8') as setting_file:
+                json.dump(self.app_setting, setting_file, ensure_ascii=False, indent=None)
+        else:
+            with open(setting_file_path, 'r',encoding = 'utf-8') as setting_file:
+                    self.app_setting = json.load(setting_file)
+    
+    def get_screens_info(self):
+        self.all_screens = QApplication.screens() # 获取所有屏幕
+        for index, screen in enumerate(self.all_screens):
+            left_top = screen.geometry().topLeft() - self.all_screens[0].geometry().topLeft()
+            right_bottom = screen.geometry().bottomRight() - self.all_screens[0].geometry().topLeft() + QPoint(1,1)
+            top_left_bottom_right_point = [left_top.x(), left_top.y(), right_bottom.x(), right_bottom.y()]
+            temp_info_dict= {
+                "select_action": None,
+                "number": index,
+                "top_left_bottom_right_point": top_left_bottom_right_point,
+                "size": screen.size(),
+                "geometry": screen.geometry(),
+                "available_geometry": screen.availableGeometry(),
+                "logical_dpi": screen.logicalDotsPerInch()
+            }
+            self.screens_info_list.append(temp_info_dict)
+            self.screens_info_in_label_text += f' 第{index+1}屏幕尺寸：({screen.size().width()}, {screen.size().height()})'
     
     def menubar_init(self):
         self.menubar = QMenuBar()
@@ -48,6 +85,24 @@ class Ui_MainWindow(QMainWindow):
         self.file_menu.addAction(self.new_menubar_action)
         self.open_menubar_action = QAction('打开', self)
         self.file_menu.addAction(self.open_menubar_action)
+        self.option_menu = self.menubar.addMenu('选项')
+        screen_select_menu = self.option_menu.addMenu('屏幕选择')
+        self.screen_select_menu_action_group = QActionGroup(self)
+        self.all_screen_select_menu = QAction('所有屏幕', self)
+        self.all_screen_select_menu.setCheckable(True)
+        self.screen_select_menu_action_group.addAction(self.all_screen_select_menu)
+        screen_select_menu.addAction(self.all_screen_select_menu)
+        for i, screen_item in enumerate(self.screens_info_list):
+            temp_action = QAction(f'第{i+1}屏幕', self)
+            temp_action.setCheckable(True)
+            self.screen_select_menu_action_group.addAction(temp_action)
+            screen_select_menu.addAction(temp_action)
+            screen_item['select_action'] = temp_action
+        self.all_screen_select_menu_action_list = screen_select_menu.actions()
+        self.all_screen_select_menu_action_list[self.app_setting['screenshot_screen']]
+        
+        # self.all_screen_select_menu = QAction('新建', self)
+        # self.screen_select_menu.addAction(self.all_screen_select_menu)
     
     def toolbar_init(self):
         self.toolbar = QToolBar()
